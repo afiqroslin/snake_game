1baabd216bbbb5b7f80cf0375571fc9c2080503a
Afiq <afiqroslin97@gmail.com>
2023-09-06 20:47:08 +0800

    add comment

--- food.py
+++ food.py
@@ -8,11 +8,11 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.color("red")
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.speed("fastest")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)    # Create a circular object with size 10px x 10px
+        self.speed("fastest")   # Set speed to fastest to make it invisible
         self.spawn()
 
-    def spawn(self):
+    def spawn(self):    # Spawn food randomly within the wall
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
